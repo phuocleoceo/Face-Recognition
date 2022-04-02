336b12b5f38141240142917cfa3f8beaeb225ee6
phuocleoceo <ht10082001@gmail.com>
2022-04-02 10:22:57 +0700

    Visualize SVM

--- Classifier/SVM.py
+++ Classifier/SVM.py
@@ -1,7 +1,9 @@
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from os.path import join, curdir
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+import seaborn as sns
 import joblib
 import json
 
@@ -9,6 +11,7 @@ import json
 class SVM:
     def __init__(self):
         self.classifier_path = join(curdir, "SVM_Model", "svm.sav")
+        self.visualization_path = join(curdir, "SVM_Model")
         # Load database chứa các vector đặc trưng
         db_path = join(curdir, "Database", "Database.json")
         with open(db_path, "r") as db:
@@ -38,8 +41,9 @@ class SVM:
         model.fit(X_train, Y_train)
 
         # Đánh giá kết quả
-        print(">> SVM training accuracy :", accuracy_score(Y_train, model.predict(X_train))*100)
-        print(">> SVM testing accuracy :", accuracy_score(Y_test, model.predict(X_test))*100)
+        Y_train_pred = model.predict(X_train)
+        Y_test_pred = model.predict(X_test)
+        self.Visualize(Y_train, Y_train_pred, Y_test, Y_test_pred)
 
         # Lưu lại model đẻ sử dụng sau này
         with open(self.classifier_path, "wb") as f:
@@ -49,3 +53,23 @@ class SVM:
         with open(self.classifier_path, "rb") as f:
             model = joblib.load(f)
         return model
+
+    def Visualize(self, Y_train, Y_train_pred, Y_test, Y_test_pred):
+        Y_train_accuracy = accuracy_score(Y_train, Y_train_pred)*100
+        Y_test_accuracy = accuracy_score(Y_test, Y_test_pred)*100
+        print(">> SVM training accuracy :", Y_train_accuracy)
+        print(">> SVM testing accuracy :", Y_test_accuracy)
+
+        plt.figure(1, figsize=(6, 4))
+        plt.title("Train data with accuracy "+str(Y_train_accuracy)+" (%)")
+        sns.heatmap(confusion_matrix(Y_train, Y_train_pred), cmap="YlGnBu", annot=True, fmt='g')
+        plt.xlabel('Predicted label')
+        plt.ylabel('True label')
+        plt.savefig(self.visualization_path+"/train_confusion.png")
+
+        plt.figure(2, figsize=(6, 4))
+        plt.title("Test data with accuracy "+str(Y_test_accuracy)+" (%)")
+        sns.heatmap(confusion_matrix(Y_test, Y_test_pred), cmap="YlGnBu", annot=True, fmt='g')
+        plt.xlabel('Predicted label')
+        plt.ylabel('True label')
+        plt.savefig(self.visualization_path+"/test_confusion.png")
