0e96a312143b23a17d61840c39564cdfac4fece2
phuocleoceo <ht10082001@gmail.com>
2022-04-14 00:45:20 +0700

    Update Dataset

---
+++ Dataset/tool.py
@@ -0,0 +1,8 @@
+from os import listdir, rename
+
+if __name__ == "__main__":
+    listPeople = listdir(f"./People/")
+    for p in listPeople:
+        listImg = listdir(f"./People/{p}")
+        for i, img in enumerate(listImg):
+            rename(f"./People/{p}/{img}", f"./People/{p}/{i+1}.jpg")
