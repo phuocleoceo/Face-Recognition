240f00d7b71cf93a15e84665b32fe79c636b3b6b
phuocleoceo <ht10082001@gmail.com>
2022-04-29 14:05:22 +0700

    SVM train test split

--- Classifier/SVM.py
+++ Classifier/SVM.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
 from os.path import join, curdir
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
@@ -16,33 +17,6 @@ class SVM:
         with open(db_path, "r") as db:
             self.database = json.load(db)
 
-    def train(self):
-        """
-        Hàm train model SVM
-        """
-        # Từ dict database, ta biến thành 2 list chữa label và embd tương ứng
-        face_labels = []
-        face_embeddings = []
-        for label, embedding in self.database.items():
-            for embd in embedding:
-                face_labels.append(label)
-                face_embeddings.append(embd)
-
-        # SVC classifier
-        model = SVC(kernel="linear", probability=True)
-
-        # Fit model
-        # Từ 1 embd ta dự đoán ra label tương ứng
-        model.fit(face_embeddings, face_labels)
-
-        # Đánh giá kết quả
-        face_pred = model.predict(face_embeddings)
-        self.Visualize(face_labels, face_pred)
-
-        # Lưu lại model đẻ sử dụng sau này
-        with open(self.classifier_path, "wb") as f:
-            joblib.dump(model, f)
-
     def load_model(self):
         """
         Hàm load SVM model để sử dụng khi nhận dạng gương mặt
@@ -51,16 +25,66 @@ class SVM:
             model = joblib.load(f)
         return model
 
-    def Visualize(self, face_labels, face_pred):
+    def split_data(self, test_size=0.2):
+        """
+        Hàm chia data thành train và test, đảm bảo số lượng công bằng giữa mỗi gương mặt
+        test_size : kích thước tập test
+        """
+        # X chứa vector đặc trưng, Y chứa nhãn gương mặt
+        X_train, X_test, Y_train, Y_test = [], [], [], []
+        # Vào database để lấy các vector gương mặt
+        for label, embedding in self.database.items():
+            # Chia thành 2 tập train, test
+            ebd_train, ebd_test = train_test_split(embedding, test_size=test_size)
+            for ebdt in ebd_train:
+                X_train.append(ebdt)
+                Y_train.append(label)
+            for ebdt in ebd_test:
+                X_test.append(ebdt)
+                Y_test.append(label)
+        return X_train, X_test, Y_train, Y_test
+
+    def train(self):
+        """
+        Hàm train model SVM
+        """
+        X_train, X_test, Y_train, Y_test = self.split_data(test_size=0.2)
+
+        # SVC classifier
+        model = SVC(kernel="linear", probability=True)
+
+        # Fit model
+        # Từ 1 embd ta dự đoán ra label tương ứng
+        model.fit(X_train, Y_train)
+
+        # Đánh giá kết quả
+        Y_train_pred = model.predict(X_train)
+        Y_test_pred = model.predict(X_test)
+        self.visualize(Y_train, Y_train_pred, Y_test, Y_test_pred)
+
+        # Lưu lại model đẻ sử dụng sau này
+        with open(self.classifier_path, "wb") as f:
+            joblib.dump(model, f)
+
+    def visualize(self, Y_train, Y_train_pred, Y_test, Y_test_pred):
         """
         Hàm trực quan hóa kết quả huấn luyện SVM
         """
-        pred_accuracy = accuracy_score(face_labels, face_pred)*100
-        print(">> SVM accuracy :", pred_accuracy)
+        Y_train_accuracy = accuracy_score(Y_train, Y_train_pred)*100
+        Y_test_accuracy = accuracy_score(Y_test, Y_test_pred)*100
+        print(">> SVM training accuracy :", Y_train_accuracy)
+        print(">> SVM testing accuracy :", Y_test_accuracy)
 
-        plt.figure(figsize=(6, 4))
-        plt.title("Train data with accuracy "+str(pred_accuracy)+" (%)")
-        sns.heatmap(confusion_matrix(face_labels, face_pred), cmap="YlGnBu", annot=True, fmt='g')
+        plt.figure(1, figsize=(7, 5))
+        plt.title("Train data with accuracy "+str(Y_train_accuracy)+" (%)")
+        sns.heatmap(confusion_matrix(Y_train, Y_train_pred), cmap="YlGnBu", annot=True, fmt='g')
         plt.xlabel('Predicted label')
         plt.ylabel('True label')
-        plt.savefig(self.visualization_path+"/svm_confusion.png")
+        plt.savefig(self.visualization_path+"/train_confusion.png")
+
+        plt.figure(2, figsize=(7, 5))
+        plt.title("Test data with accuracy "+str(Y_test_accuracy)+" (%)")
+        sns.heatmap(confusion_matrix(Y_test, Y_test_pred), cmap="YlGnBu", annot=True, fmt='g')
+        plt.xlabel('Predicted label')
+        plt.ylabel('True label')
+        plt.savefig(self.visualization_path+"/test_confusion.png")
